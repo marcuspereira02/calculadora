a6ea86656ae9daefc2d1ed498f0662b02fea2e16
Marcus Vinícius de Sá Pereira <makitasjc@outlook.com>
2025-01-14 15:26:06 -0300

    layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.marcuspereira.calculadora"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.marcuspereira.calculadora"
